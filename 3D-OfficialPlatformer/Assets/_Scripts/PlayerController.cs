cb3df7eee0ee7c96095e0fca9f2b7415142ad98a
TheoKnoll <theo@Theos-MacBook-Pro.local>
2016-03-04 20:28:48 -0700

    Added more textures and materials to the Unity game for the player and platform.

---
+++ 3D-OfficialPlatformer/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHor = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVert);
+
+		rb.AddForce (movement * speed);
+	}
+}
