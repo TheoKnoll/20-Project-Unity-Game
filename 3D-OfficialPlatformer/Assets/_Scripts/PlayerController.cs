2a590ff441e023011434105b1f0a6d1beaa93862
TheoKnoll <theo@Theos-MacBook-Pro.local>
2016-03-10 19:52:58 -0700

    Began working on the scripts for the player object to allow jumping. Currently not optimal, the jumping occasionally works but doesn't consistantly working. Unknown issue at the time. Also added a script to make the camera follow the player object.

--- 3D-OfficialPlatformer/Assets/_Scripts/PlayerController.cs
+++ 3D-OfficialPlatformer/Assets/_Scripts/PlayerController.cs
@@ -4,18 +4,48 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	private bool grounded;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		grounded = true;
 	}
-	
+
+	void OnCollisionEnter(Collision other) {
+		if(other.gameObject.CompareTag("Ground")){
+			
+				grounded = true;
+			}
+
+			}
+
+
+
 	void FixedUpdate()
 	{
 		float moveHor = Input.GetAxis ("Horizontal");
 		float moveVert = Input.GetAxis ("Vertical");
+		if(rb.transform)
+		{
 
+		}
+		if(Input.GetKeyDown("space") && grounded == true){
+			Vector3 movement = new Vector3 (moveHor, 20.0f, moveVert);
+			rb.AddForce (movement * speed);
+			grounded = false;
+		}
+		else
+		{
 		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVert);
-
 		rb.AddForce (movement * speed);
+		}
+			}
+
+	void OnCollisionExit(Collision other){
+		if(other.gameObject.CompareTag("Ground")){
+				grounded = false;
+			}
+
+					}
+
 	}
-}
