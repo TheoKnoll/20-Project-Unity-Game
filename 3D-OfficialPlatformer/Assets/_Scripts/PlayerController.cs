1bb84d436d3ee56e2e0767c6614964c8a06722ae
TheoKnoll <theo@Theos-MacBook-Pro.local>
2016-03-13 19:48:08 -0600

    Added more terrain to the first level to make it more playable. Edited the jumping script to allow for better jumps and prettier code. Fixed the code for the player to respawn, which now allows it to work. Somewhat disappointingly, the brightness of the map darkens for an unknown reason after the respawn, and this will have to be fixed later. Overall a very successful commit.

--- 3D-OfficialPlatformer/Assets/_Scripts/PlayerController.cs
+++ 3D-OfficialPlatformer/Assets/_Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	public float jumpHeight;
 	private bool grounded;
 	// Use this for initialization
 	void Start () {
@@ -11,35 +12,35 @@ public class PlayerController : MonoBehaviour {
 		grounded = true;
 	}
 
-	void OnCollisionEnter(Collision other) {
-		if(other.gameObject.CompareTag("Ground")){
-			
-				grounded = true;
-			}
-
-			}
 
 
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space) && grounded ==true){
+			Vector3 jump = new Vector3 (0.0f, jumpHeight, 0.0f);
+			rb.AddForce(jump);
+		}
+	}
 
 	void FixedUpdate()
 	{
 		float moveHor = Input.GetAxis ("Horizontal");
 		float moveVert = Input.GetAxis ("Vertical");
-		if(rb.transform)
-		{
-
-		}
-		if(Input.GetKeyDown("space") && grounded == true){
-			Vector3 movement = new Vector3 (moveHor, 20.0f, moveVert);
-			rb.AddForce (movement * speed);
-			grounded = false;
-		}
-		else
-		{
 		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVert);
 		rb.AddForce (movement * speed);
-		}
+
 			}
+
+
+	void OnCollisionEnter(Collision other) {
+		if(other.gameObject.CompareTag("Ground")){
+
+			grounded = true;
+		}
+		else if(other.gameObject.CompareTag("RespawnGame")){
+			Application.LoadLevel (Application.loadedLevel);
+		}
+
+	}
 
 	void OnCollisionExit(Collision other){
 		if(other.gameObject.CompareTag("Ground")){
@@ -47,5 +48,11 @@ public class PlayerController : MonoBehaviour {
 			}
 
 					}
+	/*void OnColliderEnter(Collider other){
+		if(other.gameObject.CompareTag("RespawnGame")){
+			Destroy (gameObject);
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}*/
 
 	}
