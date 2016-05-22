aa99cd45661cc1e6fb0515940d46eefc04d4c96b
TheoKnoll <theo@Theos-MacBook-Pro.local>
2016-05-22 17:06:52 -0600

    Added music to the game, and added controls for the music (it's toggleable via the menu)

--- 3D-OfficialPlatformer/Assets/_Scripts/DontDestroy.cs
+++ 3D-OfficialPlatformer/Assets/_Scripts/DontDestroy.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class DontDestroy : MonoBehaviour {
+	public Canvas qm;
+	public Button toggle;
+	private bool playing;
 
 	// Use this for initialization
 	void Awake () {
 		DontDestroyOnLoad (transform.gameObject);
 	}
+	void Start(){
+		qm = qm.GetComponent<Canvas> ();
+		toggle = toggle.GetComponent<Button> ();
 	
-	// Update is called once per frame
-	void Update () {
-	
+		playing = true;
+		AudioListener.pause = false;
 	}
+	// Update is called once per frame
+	public void musicToggle() {
+		if (playing) {
+			AudioListener.pause = true;
+			playing = false;
+
+		} else {
+			
+			playing = true;
+			AudioListener.pause = false;
+		}
+	}
+
 }
